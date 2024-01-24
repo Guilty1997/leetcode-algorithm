aa4d07461b5ec0a6f6abb5c6127ce188e12dd262
何洪懿 <hehongyi1997@163.com>
2024-01-24 22:55:42 +0800

    add:第2865题：美丽塔 I

---
+++ Day20240124/src/main/java/leetCode/day20240124/MaximumSumOfHeights.java
@@ -0,0 +1,34 @@
+package leetCode.day20240124;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class MaximumSumOfHeights {
+    public static void main(String[] args) {
+
+    }
+
+    public long maximumSumOfHeights(List<Integer> maxHeights) {
+        int n = maxHeights.size();
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            int pre = maxHeights.get(i);
+            long sum = pre;
+            for (int j = i - 1; j >= 0; j--) {
+                pre = Math.min(pre, maxHeights.get(j));
+                sum += pre;
+            }
+            int suf = maxHeights.get(i);
+            for (int j = i + 1; j < n; j++) {
+                suf = Math.min(suf, maxHeights.get(j));
+                sum += suf;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+
+}
