cfa501c1ec7c12a2be689ab689bf0e0670328475
何洪懿 <hehongyi1997@163.com>
2024-01-26 13:19:24 +0800

    add:第2859题：计算 K 置位下标对应元素的和

--- Day20240125/src/main/java/leetCode/day20240125/SumIndicesWithKSetBits.java
+++ Day20240125/src/main/java/leetCode/day20240125/SumIndicesWithKSetBits.java
@@ -18,22 +18,15 @@ public class SumIndicesWithKSetBits {
     }
 
     public static int sumIndicesWithKSetBits(List<Integer> nums, int k) {
-        int size = nums.size();
         int result = 0;
-        for (int i = 0; i < size; i++) {
-            int count = 0;
-            String binaryString = Integer.toBinaryString(i);
-            for (int j = 0; j < binaryString.length(); j++) {
-                if (binaryString.charAt(j) == '1') {
-                    count++;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            if (Integer.bitCount(i) == k) {
+                result += nums.get(i);
             }
-            if (count == k) {
-                result = result + nums.get(i);
-            }
-
         }
         return result;
 
     }
+
+
 }
